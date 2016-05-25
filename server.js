28acc2fc90d81c18b3c442fddaf04ff66bccb969
Benjamin Coe <ben@npmjs.com>
2016-05-24 23:00:02 -0700

    scratch: experiment with kicking off build via webhook

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ require('dotenv').config()
 var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
+var triggerBuild = require('./lib/trigger-build')
 
-app.use(bodyParser.text({
+/* app.use(bodyParser.text({
   type: 'application/json'
 }))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}))*/
 
 app.post('/hook', function (req, res) {
   var body = JSON.parse(req.body)
-
-  console.log(req.body)
+  triggerBuild(body.name)
 })
 
 var port = process.env.PORT || 5555
